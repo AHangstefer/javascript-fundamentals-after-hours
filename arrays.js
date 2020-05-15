82748c94fde87dc6bba1d3a607e77b152a74154f
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-05-14 23:47:09 -0400

    saving it

--- arrays.js
+++ arrays.js
@@ -2,35 +2,66 @@ const characters = require('./characters');
 
 /*
     Task 1 use .forEach()
-    charactersNameHeight will be an array of strings, and each string should follow this pattern: "Name: Luke Skywalker, Height: 172cm."
+    charactersNameHeight will be an array of strings, and each
+     string should follow this pattern: "Name: Luke Skywalker
+      Height: 172cm."
 */
 
+
 const charactersNameHeight = []
+characters.forEach(character=>`Name: ${character.name}, Height: ${charcter.height}.`) )
 console.log(charactersNameHeight);
 
 /*
     Task 2 use .map()
-    Using map, create a new array of strings named allCapsNames, each string following this pattern: "LUKE SKYWALKER"
+    Using map, create a new array of strings named 
+    allCapsNames, each string following this pattern:
+     "LUKE SKYWALKER"
 */
 
-const allCapsNames = []
+const allCapsNames = characters.map(character => character.name.toUpperCase())
 console.log(allCapsNames);
+
+//implied return after the arrow********
+//exactly like return character.name.toUpperCase(); 
 
 /*
     Task 3 use .filter()
-    Using filter, create a new array of objects called shortCharacters which contains only characters with under 100cm tall.
+    Using filter, create a new array of objects called 
+    shortCharacters which contains only characters with 
+    under 100cm tall.
 */
 
-const shortCharacters = []
+const shortCharacters = characters.filter(character =>{
+} if character.height <100){
+  return character
+}
 console.log(shortCharacters);
+
+//const shortCharacters = [];
+
+//character.height <100
+//return character 
+
+
+
 
 /*
     Task 4 use .reduce()
-    Using reduce, find the average number of films the characters have been in.
+    Using reduce, find the average number of films the
+     characters have been in.
 */
 
-const averageFilms = 0
+//const averageFilms = 0;
+const averageFilms = character.reduce({totalFilms, character} =>{
+return totalFilms + character.films.length;
+
+},0) / character.length;
+
 console.log(averageFilms);
+//reduce is always looking for the new value 
+
+
 
 // ==== Callbacks ====  
 
@@ -40,12 +71,22 @@ console.log(averageFilms);
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
+function functionWithStrings(a, b, cb){
+return cb(a, b);
+}
 
 /* task 2: Create several functions to callback with funWithStrings();
   * Create a function named concat that returns a single string formed from two strings.
   * Create a function named mixedCase that makes one string all caps and one string all lower case and then concatenates the 2 strings.
 */
+function concat( string1, string2){
+  return string1 + string2;
+}
 
+function mixedCase(string1, string2){
+  return string1.toLowerCase() + string2.toUpperCase;
+
+   
 /* Step 3: Check your work by un-commenting the following calls to funWithStrings(): */
 // console.log(funWithStrings('Hello, ', 'there.', concat)); // Hello, there.
 // console.log(funWithStrings('I\'m mellow.', ' I\'m crazy excited!!!', mixedCase)); // i'm mellow. I'M CRAZY EXCITED!!!
--- prototypes.js
+++ prototypes.js
@@ -5,26 +5,44 @@
 
 
 /* == Step 1: Base Constructor ==
-  Create a constructor function named CircleMaker that accepts a property for diameter
+  Create a constructor function named CircleMaker that
+   accepts a property for diameter
 */
+function CircleMaker(diameter){
+  this.diameter = diameter;
+}
 
 /* == Step 2: area Method ==
-  Create a method using CircleMaker's prototype that returns the area of a given circle.
+  Create a method using CircleMaker's prototype that returns
+   the area of a given circle.
   
   Formula for circle area: pi * radius^2
 */
 
-/* == Step 3: Circumference Method ==
-  Create another method using CircleMaker's prototype that returns the circumference of a given circle. 
+CircleMaker.prototype.area = function(){
+  return Math.PI * Math.pow((this.diameter / 2), 2);
+}
 
-  Formula for circle circumference: 2 * pi * radius
+/* == Step 3: Circumference Method ==
+  Create another method using CircleMaker's prototype that
+  returns the circumference of a given circle. 
+
+ Formula for circle circumference: 2 * pi * radius
 */
 
+CircleMaker.prototype.circumference = function(){
+  return 2*Math.PI * (this.diameter / 2);
+}
+
 /* == Step 4: Create a new object that uses CircleMaker ==
-  Create a circle object that uses the new keyword to use our CircleMaker constructor
+  Create a circle object that uses the new keyword to use
+   our CircleMaker constructor
   Add a properties and values of diameter: 5 to circle. 
 */
 
-// Test your area and circumference methods by uncommenting the logs below:
-// console.log(circle.area()); // 12.566370614359172
-// console.log(circle.circumference()); // 25.132741228718345
+const circle = new CircleMaker(5);
+
+// Test your area and circumference methods by uncommenting
+ // the logs below:
+// console.log(circle.area()); // 19
+// console.log(circle.circumference()); // 15
